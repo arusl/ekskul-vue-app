127734bfa5e430bb143fab8a8374b58583bb6b9e
Andre R <andrerusli19@gmail.com>
2021-03-11 14:21:15 +0900

    change navbar style

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
-    <b-navbar>
+    <b-navbar class="has-background-light" style="margin-bottom: 16px;">
       <template #brand>
         <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          <img
-            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-            alt="Lightweight UI components for Vue.js based on Bulma"
-          />
+          <img src="../assets/ekskul.png" alt="Ekskul Logo" />
         </b-navbar-item>
       </template>
       <template #start>
@@ -19,8 +16,8 @@
       </template>
     </b-navbar>
     <div class="container">
-        <h1 class="title">About Page</h1>
-        <!-- Content goes here -->
+      <h1 class="title">About Page</h1>
+      <!-- Content goes here -->
     </div>
   </div>
 </template>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
-    <b-navbar>
+    <b-navbar class="has-background-light" style="margin-bottom: 16px;">
       <template #brand>
         <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          <img
-            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-            alt="Lightweight UI components for Vue.js based on Bulma"
-          />
+          <img src="../assets/ekskul.png" alt="Ekskul Logo" />
         </b-navbar-item>
       </template>
       <template #start>
@@ -20,7 +17,7 @@
     </b-navbar>
     <div class="container">
       <h1 class="title">Home Page</h1>
-        <!-- Content goes here -->
+      <!-- Content goes here -->
     </div>
   </div>
 </template>
