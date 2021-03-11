758fc7b4df35ae7f3d46359bc31590711e2875cd
Andre R <andrerusli19@gmail.com>
2021-03-11 12:34:20 +0900

    initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-navbar>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+          />
+        </b-navbar-item>
+      </template>
+      <template #start>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          Home
+        </b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/about' }">
+          About
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+    <div class="container">
+        <!-- Put some contents in here, under the navigation bar-->
+        <div>Test</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import HomePage from "./components/HomePage.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+Vue.use(Buefy);
+
+const routes = [{ path: "/", component: HomePage }];
+
+const router = new VueRouter({
+  routes,
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+}).$mount("#app");
